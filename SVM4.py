fe227da92105b1dfda9d5fa5b0bcc8c694e26db9
Nonik Ika Nurfadila <102129796+Nonikika@users.noreply.github.com>
2022-07-10 23:40:06 +0700

    Create SVM4.py

---
+++ SVM4.py
@@ -0,0 +1,10 @@
+#PREDICT & EVALUATE
+
+y_pred = grid_search.predict(X_test)
+
+print((classification_report(y_test, y_pred)
+  File "C:\Users\dell\AppData\Local\Temp\ipykernel_6724\3734870688.py", line 5
+    print((classification_report(y_test, y_pred)
+                                                ^
+SyntaxError: unexpected EOF while parsing
+ 
